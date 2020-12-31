693481e89d7233abfee180c9677c867532e6f52c
ParkChan <hydrarkk@naver.com>
2020-12-31 22:45:39 +0900

    [작업내용] Observable의 생성 from/just/range/empty/interval/timer

--- app/src/main/java/com/example/testcodeapplication/ui/rx/RxJavaStep3Activity.kt
+++ app/src/main/java/com/example/testcodeapplication/ui/rx/RxJavaStep3Activity.kt
@@ -8,6 +8,7 @@ import com.example.testcodeapplication.R
 import com.example.testcodeapplication.databinding.ActivityRxJavaStep3Binding
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import java.util.concurrent.Callable
 import java.util.concurrent.Future
@@ -16,24 +17,26 @@ import java.util.concurrent.TimeUnit
 /**
  * Observer
  * Observable
- * 참고 : https://tourspace.tistory.com/279
+ * 참고 URL
+ * https://tourspace.tistory.com/279
+ * https://brunch.co.kr/@lonnie/18
  * Observable의 생성
- * from 그리고 just
+ * from/just/range/empty/interval/timer
  */
 class RxJavaStep3Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRxJavaStep3Binding
+    private val diposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_rx_java_step3)
 
-
         /**
          * from
          */
-        val list = listOf(1, 2, 3)
+        val list = arrayListOf(1, 2, 3)
         //val list = listOf(1, 2, 3).toObservable()
         val listOb = Observable.fromIterable(list)
 
@@ -64,38 +67,7 @@ class RxJavaStep3Activity : AppCompatActivity() {
 
         val futureOb = Observable.fromFuture(future)
 
-        val observer: Observer<Int> = object : Observer<Int> {
-            override fun onSubscribe(d: Disposable?) {
-                println("onSubscribe() - $d")
-            }
-
-            override fun onNext(item: Int?) {
-                println("onNext() - $item")
-            }
-
-            override fun onError(e: Throwable) {
-                println("onError() - ${e.message}")
-            }
-
-            override fun onComplete() {
-                println("onComplete()")
-            }
-        }
-//        listOb.subscribe(observer)
-//        callOb.subscribe(observer)
-//        futureOb.subscribe(observer)
-
-        /**
-         * just
-         */
-        val list2 = listOf(1, 2, 3)
-        val num = 3
-        val str = "WOW!"
-        val map = mapOf(1 to "one", 2 to "two")
-
-        val justOb = Observable.just(list2, num, str, map)
-
-        val observerJust: Observer<Any> = object : Observer<Any> {
+        val observer: Observer<Any> = object : Observer<Any> {
             override fun onSubscribe(d: Disposable?) {
                 println("onSubscribe() - $d")
             }
@@ -112,7 +84,45 @@ class RxJavaStep3Activity : AppCompatActivity() {
                 println("onComplete()")
             }
         }
-        justOb.subscribe(observerJust)
 
+
+        val list2 = listOf(1, 2, 3)
+        val num = 3
+        val str = "WOW!"
+        val map = mapOf(1 to "one", 2 to "two")
+
+        val justOb = Observable.just(list2, num, str, map)
+
+        binding.run {
+            btnFromIterable.setOnClickListener {
+                listOb.subscribe(observer)
+            }
+            btnFromCallable.setOnClickListener {
+                callOb.subscribe(observer)
+            }
+            btnFromFuture.setOnClickListener {
+                futureOb.subscribe(observer)
+            }
+            btnJust.setOnClickListener {
+                justOb.subscribe(observer)
+            }
+            btnRange.setOnClickListener {
+                Observable.range(1, 3).subscribe(observer)
+            }
+            btnEmpty.setOnClickListener {
+                Observable.empty<String>().subscribe(observer)
+            }
+            btnInterval.setOnClickListener {
+                //특정 시간 간격을 주기로 0부터 증가하는 정수 값을 발행한다.
+                val source = Observable.interval(1, TimeUnit.SECONDS)
+                source.take(3).subscribe(observer)
+            }
+            btnTimer.setOnClickListener {
+                //특정 시간 이후에 숫자 0을 발행한다. 스케줄러로 computation을 사용하고 변경 가능
+                //주어진 시간에 한번만 값을 전달
+                val source = Observable.timer(3, TimeUnit.SECONDS)
+                source.subscribe(observer)
+            }
+        }
     }
 }
