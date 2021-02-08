3df055916a5491d8ea376ae99e0f1b341a99f161
ParkChan <hydrarkk@naver.com>
2021-02-08 16:55:37 +0900

    [작업내용] 고차함수 테스트

--- TestModule/src/test/kotlin/com/example/lib/highorderfunctiontest/ArrayExamTest.kt
+++ TestModule/src/test/kotlin/com/example/lib/highorderfunctiontest/ArrayExamTest.kt
@@ -5,14 +5,19 @@ import org.junit.Test
 
 class ArrayExamTest {
 
-    private fun showResult(body: (Array<Int>, Int) -> List<Int>) {
+    private fun showResult(body: (Array<Int>, Int) -> List<Int>): List<Int> {
         val array = arrayOf(1, 2, 3, 4, 5)
         val increaseNumber = 1
-        println(body(array, increaseNumber))
+        return body(array, increaseNumber)
     }
 
     @Test
     fun `배열에 있는 값에 지정된 값 더해주기`() {
-        showResult { ints: Array<Int>, i: Int -> ints.map { it.plus(i) } }
+        val result = showResult { ints: Array<Int>, i: Int ->
+            ints.map {
+                it.plus(i)
+            }
+        }
+        println(result)
     }
 }
