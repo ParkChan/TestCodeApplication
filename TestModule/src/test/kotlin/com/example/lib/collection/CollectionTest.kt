af93c0b8b3ea96ab115ee24dfbc5eebd549349f7
ParkChan <hydrarkk@naver.com>
2021-02-08 18:11:59 +0900

    [작업내용] groupBy test

--- TestModule/src/test/kotlin/com/example/lib/collection/CollectionTest.kt
+++ TestModule/src/test/kotlin/com/example/lib/collection/CollectionTest.kt
@@ -8,14 +8,36 @@ class CollectionTest {
     private val newList1 = testList.map { "$it!" }
 
     private val newList2 = testList.flatMap {
-        println("test >>>  $it")
-        println("test >>>  ${it.toList()}")
         "$it!".toList()
     }
 
     @Test
-    fun `테스트`(){
+    fun `테스트`() {
         println(newList1)
         println(newList2)
+    }
+
+    var data = arrayOf(arrayOf("1", "2"), arrayOf("3", "4"))
+
+    @Test
+    fun `flatMap테스트`() {
+        println(data.flatMap { x -> x.toList() })
+    }
+
+    private val animals = listOf("사자", "호랑이", "코끼리")
+
+    @Test
+    fun `동물 테스트 map`() {
+        animals.map { animal -> "동물의 이름은? = $animal" }.forEach {
+            println(it)
+        }
+    }
+
+    @Test
+    fun `동물 테스트 groupBy`() {
+        animals.groupBy { animals -> if (animals.length > 2) "One" else "Two" }
+                .forEach { (key, animals) ->
+                    println("$key animals = $animals")
+                }
     }
 }
