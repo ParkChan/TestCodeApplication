feb581993a6f3215d3312552b4f6c7c73c54d224
ParkChan <hydrarkk@naver.com>
2021-08-02 11:52:40 +0900

    [추가] CoroutineStart 추가

--- TestModule/src/test/kotlin/com/example/lib/highorderfunctiontest/ArrayExamTest.kt
+++ TestModule/src/test/kotlin/com/example/lib/highorderfunctiontest/ArrayExamTest.kt
@@ -26,6 +26,5 @@ class ArrayExamTest {
             }
         }
         println(result2)
-
     }
 }
--- TestModule/src/test/kotlin/com/example/lib/highorderfunctiontest/CalculatorExamTest.kt
+++ TestModule/src/test/kotlin/com/example/lib/highorderfunctiontest/CalculatorExamTest.kt
@@ -1,6 +1,6 @@
 package com.example.lib.highorderfunctiontest
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 
 class CalculatorExamTest {
