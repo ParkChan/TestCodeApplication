ef55c572e47c4566a78925e3f93e2034e55e8227
ParkChan <hydrarkk@naver.com>
2021-08-02 15:18:40 +0900

    [추가] SupervisorJob에 대한 이해

---
+++ TestModule/src/test/kotlin/com/example/lib/coroutine/CoroutineChapter1Basic.kt
@@ -0,0 +1,51 @@
+package com.example.lib.coroutine
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import org.junit.jupiter.api.Test
+
+/*
+CoroutineContext 코루틴의 동작을 정의하는 요소 집합입니다. 다음으로 구성되어 있습니다.
+Job — 코루틴의 수명 주기를 제어합니다.
+CoroutineDispatcher — 작업을 적절한 스레드로 디스패치합니다.
+CoroutineName — 디버깅에 유용한 코루틴의 이름입니다.
+CoroutineExceptionHandler — 잡히지 않은 예외를 처리하며 시리즈의 3부에서 다룹니다.
+ */
+class CoroutineChapter1Basic {
+
+    //private val scope = CoroutineScope(Job() + Dispatchers.Main)
+    @ExperimentalCoroutinesApi
+    private val scope = TestCoroutineScope(Job() + TestCoroutineDispatcher()
+            + CoroutineName("테스트 용도의 코루틴 스레드"))
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `코루틴 실행`() {
+        val job = scope.launch{
+            try{
+                println("New coroutine!!")
+                println("You can call suspend functions")
+                println("${Thread.currentThread()}")
+            }catch (e: Throwable){
+                cancel()
+            }
+        }
+    }
+
+    @Test
+    fun `코루틴 정지`(){
+        scope.cancel()
+    }
+
+    @Test
+    fun `코루틴내에서 CoroutineContext를 변경하기`(){
+        val job = scope.launch{
+            withContext(Dispatchers.Default){
+                println("withContext New coroutine!!")
+                println("You can call suspend functions")
+                println("${Thread.currentThread()}")
+            }
+        }
+    }
+}
---
+++ TestModule/src/test/kotlin/com/example/lib/coroutine/CoroutineChapter2SupervisorJob.kt
@@ -0,0 +1,63 @@
+package com.example.lib.coroutine
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import org.junit.jupiter.api.Test
+
+/*
+일반적으로
+부모 Scope(Job)이 있고
+하위에 Child 가 4개가 있다고 가정
+Child 중하나가 실패하면 부모 코루틴에게 전파되고 모든 자식 루틴에게 전파되어 실행이 취소된다.
+그후 스레드의 기본 예외 처리기까지 전파 된다.
+
+SupervisorJob을 사용하면 발생한 예외를 부모에게 전달하지 않는다.
+다른 자식 루틴에게 전파되지 않는다.
+ */
+class CoroutineChapter2SupervisorJob {
+
+    private val coroutineScope = TestCoroutineScope(
+        Job() + TestCoroutineDispatcher()
+                + CoroutineName("SupervisorJob")
+    )
+
+    @Test
+    fun `coroutineScope에서 실행`() {
+
+        coroutineScope.launch {
+            try {
+                coroutineScope {
+                    launch {
+                        println("task 1 입니다")
+                        throw Exception()
+                    }
+                    launch { println("task 2 입니다") }
+                }
+            } catch (e: Exception) {
+                println(e)
+            }
+        }
+    }
+
+    @Test
+    fun `supervisorScope에서 실행`() {
+        coroutineScope.launch {
+            try {
+                supervisorScope {
+                    launch {
+                        try{
+                            println("task 1 입니다")
+                            throw Exception()
+                        }catch (e: Exception) {
+                            println(e)
+                        }
+                    }
+                    launch { println("task 2 입니다") }
+                }
+            } catch (e: Exception) {
+                println(e)
+            }
+        }
+    }
+}
