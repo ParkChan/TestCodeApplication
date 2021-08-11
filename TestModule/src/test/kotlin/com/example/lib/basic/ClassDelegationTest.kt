9fc05545988da1172624ea20e06e9684f3383016
ParkChan <hydrarkk@naver.com>
2021-08-11 11:03:23 +0900

    [추가] 테스트 코드 작성/클래스 위임은 어떻게 동작하는가?
    [참조](https://medium.com/til-kotlin-ko/kotlin%EC%9D%98-%ED%81%B4%EB%9E%98%EC%8A%A4-%EC%9C%84%EC%9E%84%EC%9D%80-%EC%96%B4%EB%96%BB%EA%B2%8C-%EB%8F%99%EC%9E%91%ED%95%98%EB%8A%94%EA%B0%80-c14dcbbb08ad)

---
+++ TestModule/src/test/kotlin/com/example/lib/basic/ClassDelegationTest.kt
@@ -0,0 +1,75 @@
+package com.example.lib.basic
+
+import org.junit.jupiter.api.Test
+
+class ClassDelegationTest {
+
+    interface Heater {
+        fun on()
+        fun off()
+        fun isHot() : Boolean
+    }
+
+    class ElectricHeater(var heating: Boolean = false) : Heater {
+        override fun on() {
+            println("~ ~ ~ heating ~ ~ ~")
+            heating = true
+        }
+
+        override fun off() {
+            heating = false
+        }
+
+        override fun isHot() : Boolean {
+            return heating
+        }
+    }
+
+    interface Pump {
+        fun pump()
+    }
+
+    class Thermosiphon(heater: Heater) : Pump, Heater by heater {
+        override fun pump() {
+            if (isHot()) {
+                println("=> => pumping => =>");
+            }
+        }
+    }
+
+    interface CoffeeModule {
+        fun getThermosiphon() : Thermosiphon
+    }
+
+    class MyDripCoffeeModule : CoffeeModule {
+        companion object {
+            val electricHeater: ElectricHeater by lazy {
+                ElectricHeater()
+            }
+        }
+
+        private val _thermosiphon : Thermosiphon by lazy {
+            Thermosiphon(electricHeater)
+        }
+
+        override fun getThermosiphon() : Thermosiphon = _thermosiphon
+    }
+
+    class CoffeeMaker(private val coffeeModule: CoffeeModule) {
+        fun brew() {
+            coffeeModule.getThermosiphon().run {
+                on()
+                pump()
+                println(" [_]P coffee! [_]P ")
+                off()
+            }
+        }
+    }
+
+    @Test
+    fun `CoffeeMaker 클래스 위임을 통해 유사한 방식으로 의존성을 주입`() {
+        val coffeeMaker = CoffeeMaker(MyDripCoffeeModule())
+        coffeeMaker.brew()
+    }
+
+}
