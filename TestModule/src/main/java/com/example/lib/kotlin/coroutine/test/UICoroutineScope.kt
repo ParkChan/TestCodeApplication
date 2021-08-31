9cf92d0bafd0302fc1244b31c31b186d7fbb7fda
ParkChan <hydrarkk@naver.com>
2021-08-31 17:51:28 +0900

    [수정] 사소한 코드 라인 수정

--- TestModule/src/main/java/com/example/lib/kotlin/coroutine/test/UICoroutineScope.kt
+++ TestModule/src/main/java/com/example/lib/kotlin/coroutine/test/UICoroutineScope.kt
@@ -5,7 +5,8 @@ import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
 class UICoroutineScope(
-        private val dispatchers: CoroutineContext = Dispatchers.Main) : BaseCoroutineScope {
+        private val dispatchers: CoroutineContext = Dispatchers.Main
+) : BaseCoroutineScope {
 
     override val job: Job = Job()
 
