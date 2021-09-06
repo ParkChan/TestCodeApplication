2a3330e7db4f3112fc81642815ecd3746ad8baf5
ParkChan <hydrarkk@naver.com>
2021-09-06 14:26:29 +0900

    [수정/테스트 코드] Exceptions in supervised coroutines

--- TestModule/src/test/kotlin/com/example/lib/coroutine/chapter4/CoroutineException.kt
+++ TestModule/src/test/kotlin/com/example/lib/coroutine/chapter4/CoroutineException.kt
@@ -163,4 +163,48 @@ class CoroutineException {
             secondChild.join()
         }
     }
+
+    @DelicateCoroutinesApi
+    @Test
+    fun `Supervision scope`() = runBlocking {
+        try {
+            supervisorScope {
+                val child = launch {
+                    try {
+                        println("The child is sleeping")
+                        delay(Long.MAX_VALUE)
+                    } finally {
+                        println("The child is cancelled")
+                    }
+                }
+                // Give our child a chance to execute and print using yield
+                yield()
+                println("Throwing an exception from the scope")
+                throw AssertionError()
+            }
+        } catch (e: AssertionError) {
+            println("Caught an assertion error")
+        }
+    }
+
+    @DelicateCoroutinesApi
+    @Test
+    fun `Exceptions in supervised coroutines`() = runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("CoroutineExceptionHandler got $exception")
+        }
+        supervisorScope {
+            val child = launch(handler) {
+                println("The child throws an exception")
+                throw AssertionError()
+            }
+            println("The scope is completing")
+        }
+        println("The scope is completed")
+    }
+
+    //The child throws an exception
+    //CoroutineExceptionHandler got The child throws an exception
+    //The scope is completing
+    //The scope is completed
 }
