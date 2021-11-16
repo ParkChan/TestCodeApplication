6a51d47c8b5d0fae0b6ea3380ce80ed1893ecd96
ParkChan <hydrarkk@naver.com>
2021-11-16 21:14:41 +0900

    [테스트코드] kotlin in(반공변):Write-Only / out(공변):Read-Only
    [테스트코드] kotlin in(반공변):Write-Only / out(공변):Read-Only
    
    Update README.md
    [테스트코드] kotlin in(반공변):Write-Only / out(공변):Read-Only

---
+++ TestModule/src/main/java/com/example/lib/java/BasicTestCode.java
@@ -0,0 +1,8 @@
+package com.example.lib.java;
+
+public class BasicTestCode {
+
+    public static void main(String[] args){
+        System.out.println("Test");
+    }
+}
---
+++ TestModule/src/test/kotlin/com/example/lib/basic/GenericTest.kt
@@ -0,0 +1,37 @@
+package com.example.lib.basic
+
+import org.junit.jupiter.api.Test
+
+//kotlin
+// in 반공변:Write-Only
+// out 공변:Read-Only
+/**
+ * 참조 : https://readystory.tistory.com/201
+ * 참조 : https://medium.com/@joongwon/java-java%EC%9D%98-generics-604b562530b3
+ */
+class GenericTest {
+
+    open class Fruit
+    class Apple : Fruit()
+    class Banana : Fruit()
+    
+    @Test
+    fun `배열로 테스트합니다`(){
+        val fruits: Array<Apple> = arrayOf(Apple())
+        //receiveFruits(fruits)// 제네릭에 대한 타입불변성으로 인한 컴파일 에러
+    }
+
+    fun receiveFruits(fruits: Array<Fruit>) {
+        println("Number of fruits: ${fruits.size}")
+    }
+
+    @Test
+    fun `리스트로 테스트`(){
+        val fruits: List<Apple> = listOf(Apple())   //List<out E> 공변타입 read 허용
+        receiveFruits2(fruits)
+    }
+
+    fun receiveFruits2(fruits: List<Fruit>) {
+        println("Number of fruits: ${fruits.size}")
+    }
+}
