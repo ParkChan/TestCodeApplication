3a096037ae7c81bb4d7c5837d45758a5b1445e4d
ParkChan <hydrarkk@naver.com>
2021-12-02 14:50:14 +0900

    [테스트코드] - flatMapLatest : 이전에 대기중이거나 동작중인 flow는 cancel

--- TestModule/src/test/kotlin/com/example/lib/coroutine/chapter5/FlowTest.kt
+++ TestModule/src/test/kotlin/com/example/lib/coroutine/chapter5/FlowTest.kt
@@ -1,5 +1,6 @@
 package com.example.lib.coroutine.chapter5
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.debug.junit5.CoroutinesTimeout
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
@@ -28,11 +29,12 @@ class FlowTest {
         delay(100)
     }
 
-    val intFlow = flowOf(1, 2, 3).onEach { delay(10) }
-    val charFlow = flowOf("A", "B", "C","D","E").onEach { delay(20) }
-    val emptyFlow = flowOf(emptyList<String>())
+    private val intFlow = flowOf(1, 2, 3).onEach { delay(10) }
+    private val charFlow = flowOf("A", "B", "C", "D", "E").onEach { delay(20) }
+    private var emptyFlow = emptyFlow<String>()
+
     @Test
-    fun `combine 테스트 두 개의 Flow를 결합해서 하나의 결과를 만들고 싶을 때 combine을 사용합니다`() = runBlocking {
+    fun `두 개의 Flow를 결합해서 하나의 결과를 만들고 싶을 때 combine을 사용합니다`() = runBlocking {
         println("Start")
         val startTime = System.currentTimeMillis()
         intFlow.combine(charFlow) { num, character ->
@@ -45,10 +47,12 @@ class FlowTest {
     }
 
     @Test
-    fun `combine flow중 하나라도 emptyFlow면 수행이 안 되지만 빈 값을 전달하여 수행하도록 합니다`() = runBlocking {
+    fun `combine 의 특성을 테스트 합니다`() = runBlocking {
+        //두개의 flow 에서 서로 다른 타이밍으로 값이 방출될때 최신값만을 기준으로 두개의 방출값을 연산함
+        //둘중 하나라도 값이 없으면 combine이 수행되지 않음
         println("Start")
         val startTime = System.currentTimeMillis()
-        intFlow.combine(emptyFlow) { num, character ->
+        intFlow.combine(charFlow) { num, character ->
             "$num / $character"
         }.collect {
             println(it)
@@ -58,7 +62,9 @@ class FlowTest {
     }
 
     @Test
-    fun `zip 테스트 combine과 다른 점은 두 Flow의 방출 값을 모아서 한 번에 방출`() = runBlocking {
+    fun `zip 의 특성을 테스트 합니다 `() = runBlocking {
+        //flow 가 개수가 다르면 적은 개수에 맞춰서 출력됨
+        //두개의 flow 의 방출 타이밍이 서로 다르면 느린 flow의 방출값을 기다림
         intFlow.zip(charFlow) { num, character ->
             "$num / $character"
         }.collect {
@@ -67,11 +73,32 @@ class FlowTest {
     }
 
     @Test
-    fun `flattenMerge 테스트 다른 방출 값과 관계도 없으며, 여러 개의 Flow들의 방출 값이 하나의 Flow의 방출`() = runBlocking {
+    fun `flattenMerge 다른 방출 값과 관계도 없으며, 여러 개의 Flow들의 방출 값이 하나의 Flow의 방출`() = runBlocking {
         val startTime = System.currentTimeMillis()
         flowOf(intFlow, charFlow).flattenMerge().collect {
             println("$it")
         }
         println("실행 시간 ${System.currentTimeMillis() - startTime}")
     }
+
+    @Test
+    fun `flatMapLatest 이전에 대기중이거나 동작중인 flow는 cancel`() = runBlocking {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow().onEach { delay(100) }
+            .flatMapLatest { requestFlow(it) }.collect { value ->
+                println("$value at ${System.currentTimeMillis() - startTime} ms from start")
+            }
+    }
+
+    private fun requestFlow(i: Int): Flow<String> = flow {
+        try{
+            emit("$i: First")
+            delay(500)
+            emit("$i: Second")
+        }catch (cancel: CancellationException){
+            println("cancelled!!")
+        }
+    }
+
+
 }
