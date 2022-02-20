d38ac572235ef0c03d7a710dc87f12d240d90130
ParkChan <hydrarkk@gmail.com>
2022-02-20 22:37:38 +0900

    [테스트코드] 코루틴 취소 테스트 코드

--- TestModule/src/test/kotlin/com/example/lib/coroutine/test/CoroutineCancellation.kt
+++ TestModule/src/test/kotlin/com/example/lib/coroutine/test/CoroutineCancellation.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 class CoroutineCancellation {
 
     @Test
-    fun `공식문서 예제 정상적인 코루틴 cancel() 동작 테스트`() = runBlocking{
+    fun `공식문서 예제 정상적인 코루틴 cancel() 동작 테스트`() = runBlocking {
         val job = launch {
             repeat(1000) { i ->
                 println("job: I'm sleeping $i ...")
@@ -22,10 +22,11 @@ class CoroutineCancellation {
         job.join() // waits for job's completion
         println("main: Now I can quit.")
     }
+
     @Test
-    fun `동작이 멈추지 않고 계속해서 실행되는 경우`() = runBlocking{
+    fun `동작이 멈추지 않고 계속해서 실행되는 경우`() = runBlocking {
         val job = launch {
-            while(true) {
+            while (true) {
                 println("while")
             }
         }
@@ -35,10 +36,11 @@ class CoroutineCancellation {
         job.join() // waits for job's completion
         println("main: Now I can quit.")
     }
+
     @Test
-    fun `메인 쓰레드를 Dispatcher Default로 변경하면 동작은 바뀌지만 끝나지 않는것은 동일`() = runBlocking{
+    fun `메인 쓰레드를 Dispatcher Default로 변경하면 동작은 바뀌지만 끝나지 않는것은 동일`() = runBlocking {
         val job = launch(Dispatchers.Default) {
-            while(true) {
+            while (true) {
             }
         }
         delay(1000L) // delay a bit
