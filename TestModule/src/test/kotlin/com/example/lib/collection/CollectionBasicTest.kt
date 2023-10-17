8b1ab61ba94bf52f13ef1875eab09c4b6295ecf0
ParkChan <hydrarkk@naver.com>
2023-10-18 02:19:22 +0900

    [추가] reduce 와 fold

--- TestModule/src/test/kotlin/com/example/lib/collection/CollectionBasicTest.kt
+++ TestModule/src/test/kotlin/com/example/lib/collection/CollectionBasicTest.kt
@@ -110,4 +110,56 @@ class CollectionBasicTest {
         print("originData ${data.toList()}")
         println()
     }
+
+    @Test
+    fun `reduce 와 fold 기본 예제`(){
+        val numbers = (1..10).toList()
+
+        // total의 초기값은 numbers[1]로 시작한다.
+        val sumUsingReduce = numbers.reduce {total, num ->
+            println("$total $num")
+            total + num
+        }
+        println(sumUsingReduce) // 55
+
+        // total의 초기값은 100으로 시작한다.
+        val sumUsingFold = numbers.fold(100) { total, num ->
+            total + num
+        }
+        println(sumUsingFold) // 155
+    }
+
+    @Test
+    fun `reduce 와 fold 예외 상황`(){
+        val numbers = (1..5).toList()
+
+        // total의 초기값은 numbers[1]로 시작한다.
+        val sumUsingReduce = numbers.reduce {total, num ->
+            total + num * 2
+        }
+        println(sumUsingReduce) // 29(?)
+        //reduce는 다음 코드와 같이 초기값을 첫번째 요소로 정해놓고 다음 요소부터 연산을 시작한다
+
+        // total의 초기값은 0으로 시작한다.
+        val sumUsingFold = numbers.fold(0) { total, num ->
+            total + num * 2
+        }
+        println(sumUsingFold) // 30
+    }
+
+    @Test
+    fun `reduce 와 fold Empty 리스트에 따른 반환값`(){
+
+        // total의 초기값은 numbers[1]로 시작한다.
+        val sumUsingReduce = intArrayOf().reduce {total, num ->
+            total + num
+        }
+        println(sumUsingReduce) // java.lang.UnsupportedOperationException: Empty array can't be reduced.
+
+        // total의 초기값은 0으로 시작한다.
+        val sumUsingFold = intArrayOf().fold(11) { total, num ->
+            total + num
+        }
+        println(sumUsingFold)
+    }
 }
